463157ace3cc671ae57d3a1b8cdf671856d9f369
HOA2k1 <phuoghoa2k1@gmail.com>
2021-10-14 19:44:30 +0700

    Update JobRequestController.php

--- app/Http/Controllers/api/JobRequestController.php
+++ app/Http/Controllers/api/JobRequestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Candidate;
 use App\Models\JobRequest;
 use Illuminate\Http\Request;
 
@@ -28,6 +29,7 @@ class JobRequestController extends Controller
     
     public function remove($id)
     {
+        Candidate::where('job_id', 'like', $id)->delete();
         JobRequest::destroy($id);
         return redirect(route('jobrequest'));
     }

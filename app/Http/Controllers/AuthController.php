1c2e153dbd4c3ae2415483ac670b01f8b3a92236
HOA2k1 <phuoghoa2k1@gmail.com>
2021-11-13 14:27:02 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -56,8 +56,8 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60,
-            'user' => auth()->user()
+            'expires_in' => auth()->factory()->getTTL() * 60,            
+            'user' => User::with('roles')->where('id', auth()->user()->id)->first(),
         ]);
     }
 

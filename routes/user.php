be38b732fb955668833f18804f8462c5f37a28e3
cuongtt <cuongttph12174@fpt.edu.vn>
2021-12-11 08:51:18 +0700

    update router user

--- routes/user.php
+++ routes/user.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth.jwt', 'can:hrManager,App\User'])
     ->group(function (){
         Route::prefix('user')->group(function (){
-            Route::get('user-detail/{user}', [\App\Http\Controllers\api\UserController::class,'show']);
             Route::post('regsiter-member', [\App\Http\Controllers\api\UserController::class,'store']);
             Route::delete('destroy/{user}', [\App\Http\Controllers\api\UserController::class,'destroy']);
             Route::post('update-member/{user}', [\App\Http\Controllers\api\UserController::class,'update']);
@@ -17,4 +16,5 @@ Route::middleware(['auth.jwt', 'can:hrManager,App\User'])
 
 Route::get('user/list-role',[\App\Http\Controllers\api\UserController::class,'listRoleUserLogin'])->middleware('auth.jwt');
 Route::post('user/update-profile-login',[\App\Http\Controllers\api\UserController::class,'updateProfile'])->middleware('auth.jwt');
-Route::get('list-user', [\App\Http\Controllers\api\UserController::class,'index'])->middleware('auth.jwt');
+Route::get('user/list-user', [\App\Http\Controllers\api\UserController::class,'index'])->middleware('auth.jwt');
+Route::get('user/user-detail/{user}', [\App\Http\Controllers\api\UserController::class,'show'])->middleware('auth.jwt');

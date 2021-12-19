56e5a18aa9b786e93db0ad80ff9a0f9f8ca07388
HOA2k1 <phuoghoa2k1@gmail.com>
2021-12-19 10:11:53 +0700

    Update CandidateFormRequest.php

--- app/Http/Requests/CandidateFormRequest.php
+++ app/Http/Requests/CandidateFormRequest.php
@@ -70,6 +70,7 @@ class CandidateFormRequest extends FormRequest
             'cv.mimes' => 'Cv chỉ nhận định dạng pdf/doc/docx',
             'email.required' => 'Không được để trống trường này',
             'email.email' => 'Không đúng định dạng email',
+            'email.unique' => 'Ứng viên đã tồn tại',
             'name.min' => 'Nhập ít nhất 4 kí tự',
             'phone.required' => 'Không được để trống trường này',
             'source.required' => 'Không được để trống trường này',

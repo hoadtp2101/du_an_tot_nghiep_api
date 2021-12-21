7716474796f8cde9acdf56b362967f3513fcfca3
cuongtt <cuongttph12174@fpt.edu.vn>
2021-12-21 20:57:22 +0700

    update api

--- routes/jobrequest.php
+++ routes/jobrequest.php
@@ -10,6 +10,6 @@ Route::group(['middleware' => 'auth.jwt'], function(){
     Route::post('/jobrequest/create', [JobRequestController::class, 'create'])->name('jobrequest.create')->middleware('can:create,App\JobRequest');
     Route::post('/jobrequest/edit/{jobRequest}', [JobRequestController::class, 'update'])->name('jobrequest.update')->middleware('can:update,jobRequest');
     Route::post('/jobrequest/approve/{jobRequest}', [JobRequestController::class, 'approve'])->name('jobrequest.approve')->middleware('can:approve,jobRequest');
-    Route::delete('/jobrequest/delete/{jobRequest}', [JobRequestController::class, 'remove'])->name('jobrequest.delete')->middleware('can:update,jobRequest');
+    Route::delete('/jobrequest/delete/{jobRequest}', [JobRequestController::class, 'remove'])->name('jobrequest.delete')->middleware('can:delete,jobRequest');
 });
 Route::get('pdf/{id}', [JobRequestController::class, 'pdf']);
